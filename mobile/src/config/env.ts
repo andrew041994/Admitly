ef47be7dd178ca8be003ee3ec02bbc98a2ae5618
andrew041994 <andrew041994@gmail.com>
2026-04-07 20:19:12 -0400

    Phase 2 patch: mobile authentication UI/screens

--- mobile/src/config/env.ts
+++ mobile/src/config/env.ts
@@ -3,5 +3,5 @@ import Constants from 'expo-constants';
 const extras = Constants.expoConfig?.extra ?? {};
 
 export const env = {
-  apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL ?? (extras.apiBaseUrl as string) ?? 'http://localhost:8000',
+  apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL ?? (extras.apiBaseUrl as string) ?? 'http://192.168.1.123:8000',
 };
