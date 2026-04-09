7f2d71406306a1d0a670f06e9dc7dc1edf439077
andrew041994 <andrew041994@gmail.com>
2026-04-09 01:09:24 -0400

    fix: set python version for render

--- mobile/src/config/env.ts
+++ mobile/src/config/env.ts
@@ -3,6 +3,9 @@ import Constants from 'expo-constants';
 const extras = Constants.expoConfig?.extra ?? {};
 
 export const env = {
-  apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL ?? (extras.apiBaseUrl as string) ?? 'https://admitly.onrender.com',
+  apiBaseUrl:
+  process.env.EXPO_PUBLIC_API_BASE_URL ??
+  (extras.apiBaseUrl as string) ??
+  'https://admitly.onrender.com',
   enableDevTestCheckout: (process.env.EXPO_PUBLIC_ENABLE_DEV_TEST_CHECKOUT ?? (extras.enableDevTestCheckout as string) ?? 'false') === 'true',
 };
